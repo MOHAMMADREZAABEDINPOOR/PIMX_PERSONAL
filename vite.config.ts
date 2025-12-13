74a936482bb0f6e55a667abb2adf63eb1fd34c09
MOHAMMADREZAABEDINPOOR <mohammadrezaabedinpoor@example.com>
2025-12-14 00:28:04 +0330

    Add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/', // مهم برای Cloudflare Pages
+      base: '/PIMX_PERSONAL/', // مهم برای GitHub Pages
       server: {
         port: 8000,
         host: '0.0.0.0',
